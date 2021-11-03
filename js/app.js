62c1cca6d235a0ebeab5a2e78cad7b81b8bf75f4
Pierre-Loup Perez <devipilou@gmail.com>
2021-11-03 12:32:52 +0100

    add footer menu

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@ console.log("callback - tsparticles config loaded");
 .catch((error) => {
 console.error(error);
 });
+
+let age = document.querySelector('#age');
+let annee = document.querySelector('#annee');
+
+annee.innerText = (new Date()).getFullYear();
+age.innerText = annee.innerText - 1986;
