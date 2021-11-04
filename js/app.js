85c12c58a724dd43226a83121cb46c6383a71866
Pierre-Loup Perez <devipilou@gmail.com>
2021-11-04 13:58:25 +0100

    small correction and presentation content

--- js/app.js
+++ js/app.js
@@ -8,6 +8,16 @@ console.error(error);
 
 let age = document.querySelector('#age');
 let annee = document.querySelector('#annee');
+let modifAge = 0
 
 annee.innerText = (new Date()).getFullYear();
-age.innerText = annee.innerText - 1986;
+
+if ((new Date()).getMonth() < 3){
+modifAge = 1;
+}else if ((new Date()).getMonth() == 3){
+    if ((new Date()).getDate() < 18){
+        modifAge = 1;
+    }
+};
+
+age.innerText = annee.innerText - 1986 - modifAge;
